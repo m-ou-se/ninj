bcde93362b93140b2d2f423549c50d938c1edf49
Mara Bos <m-ou.se@m-ou.se>
2018-09-08 22:42:42 +0200

    Clean up main file.

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,6 @@ use ninj::spec::read;
 fn main() {
 	match read("build.ninja".as_ref()) {
 		Ok(result) => println!("spec: {:#?}", result),
-		Err(error) => {
-			println!("{:#?}", error);
-			println!("{}", error);
-		}
+		Err(error) => println!("{}", error),
 	}
 }
