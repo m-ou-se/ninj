6bce362f4c8be61c5d8a9c1628d57e679fa03628
Sjors Gielen <sjors@sjorsgielen.nl>
2019-01-19 22:10:07 +0100

    Improve display of the current progress.
    This patch adds the -P / --progress option to ninj, which sets the type of
    progress display. There are currently four options: no progress display (none),
    display in text (text), display a progress bar in ASCII (ascii) or use fancy
    Unicode characters (highres).
    
    The term_size dependency is added for establishing the width of the terminal.
    
    A bug is fixed causing the final time estimation to be unknown, caused by all
    workers being Done.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod timeformat;
 mod worker;
 
 use self::logger::Logger;
-use self::status::{show_build_status, BuildStatus};
+use self::status::{show_build_status, BuildStatus, ProgressFormat};
 use self::worker::Worker;
 use log::{debug, error};
 use ninj::buildlog::BuildLog;
@@ -60,6 +60,10 @@ struct Options {
 	/// Enable debug messages.
 	#[structopt(long)]
 	debug: bool,
+
+	/// Set format of progress indication (none/text/ascii/highres).
+	#[structopt(short = "P", long = "progress", default_value = "highres")]
+	progress: ProgressFormat,
 }
 
 fn main() {
@@ -199,6 +203,7 @@ fn main() {
 				&spec,
 				&build_log,
 				opt.sleep_run,
+				opt.progress,
 			);
 		}
 	})
--- src/status.rs
+++ src/status.rs
@@ -3,6 +3,7 @@ use crate::worker::StatusUpdater;
 use ninj::buildlog::BuildLog;
 use ninj::queue::{AsyncBuildQueue, TaskStatus};
 use ninj::spec::Spec;
+use std::fmt;
 use std::sync::{Condvar, Mutex};
 use std::time::{Duration, Instant};
 use time::Duration as TimeDuration;
@@ -103,6 +104,120 @@ fn estimated_total_task_time(
 	}
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum ProgressFormat {
+	None,
+	Text,
+	ASCIIBar,
+	HighResBar,
+}
+
+#[derive(Debug)]
+pub struct ParseProgressFormatError {
+	value: String,
+}
+
+impl core::str::FromStr for ProgressFormat {
+	type Err = ParseProgressFormatError;
+	fn from_str(s: &str) -> Result<Self, ParseProgressFormatError> {
+		let s = s.to_lowercase();
+		if s == "none" {
+			Ok(ProgressFormat::None)
+		} else if s == "text" {
+			Ok(ProgressFormat::Text)
+		} else if s == "ascii" {
+			Ok(ProgressFormat::ASCIIBar)
+		} else if s == "highres" {
+			Ok(ProgressFormat::HighResBar)
+		} else {
+			Err(ParseProgressFormatError { value: s })
+		}
+	}
+}
+
+impl fmt::Display for ParseProgressFormatError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", self.value)
+	}
+}
+
+fn render_progress_bar(width: usize, percentage: f32, text: &String, highres: bool) -> String {
+	while text.len() > width {
+		return String::from(text.split_at(width).0);
+	}
+
+	let textwidth = text.len();
+	let spaceleft = (width - textwidth) / 2;
+	let spaceright = width - textwidth - spaceleft;
+
+	let arrow_width = percentage * width as f32 / 100.;
+	let arrow_full_char = if highres { "▉" } else { "=" };
+	let arrow_cut_char = if highres { "▋" } else { arrow_full_char };
+
+	let arrow_rest = arrow_width - arrow_width.floor();
+	let arrow_rest_char = if arrow_rest < 0.01 {
+		""
+	} else if !highres {
+		">"
+	} else if arrow_rest > 0.84 {
+		"▊"
+	} else if arrow_rest > 0.68 {
+		"▋"
+	} else if arrow_rest > 0.52 {
+		"▌"
+	} else if arrow_rest > 0.36 {
+		"▍"
+	} else if arrow_rest > 0.20 {
+		"▎"
+	} else {
+		"▏"
+	};
+
+	// If highres, the arrow characters are all 3 bytes. This is assumed below.
+	let arrow_charlen = if highres { 3 } else { 1 };
+	assert!(arrow_full_char.len() == arrow_charlen);
+	assert!(arrow_rest_char.is_empty() || arrow_rest_char.len() == arrow_charlen);
+
+	let arrow = arrow_full_char.repeat(arrow_width.floor() as usize) + arrow_rest_char;
+	let arrowlen = arrow.chars().count();
+
+	let left = if arrowlen < spaceleft {
+		format!("{}{}", arrow, " ".repeat(spaceleft - arrowlen))
+	} else {
+		format!(
+			"{}{}",
+			arrow.split_at((spaceleft - 1) * arrow_charlen).0,
+			arrow_cut_char
+		)
+	};
+	let lefttextlen = spaceleft + textwidth;
+	let right = if arrowlen < lefttextlen {
+		" ".repeat(spaceright)
+	} else if arrowlen == width {
+		format!(
+			"{}{}",
+			arrow
+				.split_at(lefttextlen * arrow_charlen)
+				.1
+				.split_at((spaceright - 1) * arrow_charlen)
+				.0,
+			arrow_cut_char
+		)
+	} else {
+		format!(
+			"{}{}",
+			arrow.split_at(lefttextlen * arrow_charlen).1,
+			" ".repeat(width - arrowlen)
+		)
+	};
+
+	assert!(left.chars().count() == spaceleft);
+	assert!(right.chars().count() == spaceright);
+	assert!(spaceleft + text.chars().count() + spaceright == width);
+
+	format!("\x1b[32m{}\x1b[m{}\x1b[32m{}\x1b[m", left, text, right)
+}
+
 pub fn show_build_status(
 	start_time: Instant,
 	status: &BuildStatus,
@@ -110,6 +225,7 @@ pub fn show_build_status(
 	spec: &Spec,
 	build_log: &Mutex<BuildLog>,
 	sleep: bool,
+	progress_format: ProgressFormat,
 ) {
 	let mut lock = status.inner.lock().unwrap();
 	println!("{}:", if sleep { "Sleeping" } else { "Building" });
@@ -181,11 +297,11 @@ pub fn show_build_status(
 				}
 			}
 
-			// All workers still idle? Nothing else to do, stop simulating
+			// All workers still idle or done? Nothing else to do, stop simulating
 			if buildstate
 				.workers
 				.iter()
-				.find(|&w| *w != WorkerStatus::Idle)
+				.find(|&w| *w != WorkerStatus::Idle && *w != WorkerStatus::Done)
 				.is_none()
 			{
 				break;
@@ -224,6 +340,8 @@ pub fn show_build_status(
 			{
 				Some(earliest_remaining_job) => earliest_remaining_job,
 				None => {
+					// This happens when workers are working on a job for which we cannot
+					// guess how long it will take.
 					estimation_impossible = true;
 					break;
 				}
@@ -247,44 +365,95 @@ pub fn show_build_status(
 
 		let now = Instant::now();
 
-		if estimation_impossible || simulated_time <= now {
-			println!(
-				"Building for {}, estimating remaining time...\x1b[K\x1b[m",
-				MinSec::since(start_time)
-			);
+		let current_duration = start_time.elapsed();
+		let remaining_duration = if estimation_impossible {
+			None
+		} else if simulated_time < now {
+			// Should have been done already, estimate it will be done immediately
+			Some(Duration::from_millis(0))
 		} else {
-			let remaining_duration = simulated_time - now;
-			let eta = TimeDuration::from_std(remaining_duration)
-				.map(|duration| chrono::Local::now() + duration);
-			match eta {
-				Ok(eta) => {
-					let is_soon = eta.date() == chrono::Local::now().date()
-						&& remaining_duration < Duration::from_secs(8 * 3600);
-					let timeformat = if is_soon {
-						"%H:%M:%S"
+			Some(simulated_time - now)
+		};
+
+		let (percentage, text) = match remaining_duration {
+			None => (0., format!("??% (estimating time)")),
+			Some(remaining_duration) => {
+				let percentage = (100 * as_millis(current_duration)) as f32
+					/ as_millis(current_duration + remaining_duration) as f32;
+
+				// Every 5 seconds, switch between showing ETA and remaining duration
+				let show_eta = (current_duration.as_secs() % 10) > 5;
+
+				(
+					percentage,
+					if show_eta {
+						let eta = TimeDuration::from_std(remaining_duration)
+							.map(|duration| chrono::Local::now() + duration)
+							.unwrap();
+						let is_soon = eta.date() == chrono::Local::now().date()
+							&& remaining_duration < Duration::from_secs(8 * 3600);
+						let timeformat = if is_soon {
+							"%H:%M:%S"
+						} else {
+							"%Y-%m-%d %H:%M:%S"
+						};
+						format!(
+							"{}% (ETA {})",
+							percentage.ceil() as u8,
+							eta.format(timeformat)
+						)
 					} else {
-						"%Y-%m-%d %H:%M:%S"
-					};
-					println!(
-						"Building for {}, remaining time for build is {} (ETA {})\x1b[K\x1b[m",
-						MinSec::since(start_time),
-						MinSec::from_duration(remaining_duration),
-						eta.format(timeformat)
-					);
-				}
-				_ => println!(
-					"Building for {}, remaining time is infinite...\x1b[K\x1b[m",
-					MinSec::since(start_time)
-				),
+						format!(
+							"{}% ({} remaining)",
+							percentage.ceil() as u8,
+							MinSec::from_duration(remaining_duration)
+						)
+					},
+				)
 			}
-		}
+		};
+
+		let progress = match progress_format {
+			ProgressFormat::None => "".to_owned(),
+			ProgressFormat::Text => format!(
+				"[Building for {}, {}]\x1b[K\x1b[m\n",
+				MinSec::since(start_time),
+				text
+			),
+			ProgressFormat::ASCIIBar | ProgressFormat::HighResBar => format!(
+				"[{}]\x1b[K\x1b[m\n",
+				render_progress_bar(
+					terminal_width() - 3,
+					percentage,
+					&text,
+					progress_format == ProgressFormat::HighResBar
+				)
+			),
+		};
+
+		print!("{}", progress);
 
 		if build_is_done {
 			break;
 		}
 
-		print!("\x1b[{}A", buildstate.workers.len() + 1);
+		print!(
+			"\x1b[{}A",
+			buildstate.workers.len() + progress.lines().count()
+		);
 		lock = status.inner.lock().unwrap();
 	}
 	println!("\x1b[32;1mFinished.\x1b[m");
+}
+
+fn terminal_width() -> usize {
+	if let Some((w, _)) = term_size::dimensions() {
+		w
+	} else {
+		80 /* an educated guess */
+	}
+}
+
+fn as_millis(d: Duration) -> u64 {
+	d.as_secs() * 1000 + u64::from(d.subsec_millis())
 }
