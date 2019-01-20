9381686b7f1094d88ab9e69f22a09f998be6b618
Mara Bos <m-ou.se@m-ou.se>
2019-01-20 01:07:29 +0100

    Improve display of progress bar.

--- src/status/progressbar.rs
+++ src/status/progressbar.rs
@@ -9,43 +9,38 @@ pub struct ProgressBar<'a> {
 
 impl<'a> std::fmt::Display for ProgressBar<'a> {
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		if self.label.len() > self.width {
-			f.write_str(&self.label[..self.width])?;
-			return Ok(());
-		}
-
-		let fancy_blocks = [' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉'];
-
-		let ticks = (self.progress * self.width as f64 * fancy_blocks.len() as f64) as usize;
-		let label_pos = (self.width - self.label.len()) / 2;
+		let blocks = [' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉'];
+		let ticks = (self.progress * self.width as f64 * blocks.len() as f64) as usize;
+		let label_pos = self.width.saturating_sub(self.label.len()) / 2;
 		f.write_str("\x1b[32m")?;
 		let mut i = 0;
 		while i < self.width {
-			if i == label_pos && !self.label.is_empty() {
-				f.write_str("\x1b[m")?;
-				f.write_str(self.label)?;
-				f.write_str("\x1b[32m")?;
-				i += self.label.len();
+			if i >= label_pos && i < label_pos + self.label.len() {
+				f.write_str(if i < ticks / blocks.len() {
+					"\x1b[32;7m"
+				} else {
+					"\x1b[m"
+				})?;
+				f.write_str(&self.label[i - label_pos..][..1])?;
+				f.write_str("\x1b[27;32m")?;
 			} else {
-				f.write_char(if i < ticks / fancy_blocks.len() {
+				f.write_char(if i < ticks / blocks.len() {
 					if self.ascii {
 						'='
-					} else if i + 1 == label_pos {
-						fancy_blocks[5]
 					} else {
-						fancy_blocks[7]
+						'█'
 					}
-				} else if i == ticks / fancy_blocks.len() {
+				} else if i == ticks / blocks.len() {
 					if self.ascii {
 						'>'
 					} else {
-						fancy_blocks[ticks % 8]
+						blocks[ticks % 8]
 					}
 				} else {
 					' '
 				})?;
-				i += 1;
 			}
+			i += 1;
 		}
 		f.write_str("\x1b[m")?;
 		Ok(())
