64aa570011fffe45343095419bf3a676af9d2c4c
Mara Bos <m-ou.se@m-ou.se>
2020-03-29 16:52:51 +0200

    Fix ninj -t deps.
    The valid/stale-check was wrong.

--- src/subtools/deps.rs
+++ src/subtools/deps.rs
@@ -11,23 +11,20 @@ pub(super) fn main(opt: &Options) -> Result<(), Error> {
 	let dep_log = DepLogMut::open(spec.build_dir().join(".ninja_deps"))?;
 	for (path, deps) in dep_log.iter() {
 		if targets.contains_key(&path[..]) {
-			let mtime = || {
-				std::fs::metadata(path.as_path())
-					.and_then(|m| m.modified())
-					.ok()
-					.map(Timestamp::from_system_time)
-			};
-			let nanos = deps.mtime().map_or(0, Timestamp::to_nanos);
+			let mtime = std::fs::metadata(path.as_path())
+				.and_then(|m| m.modified())
+				.ok()
+				.map(Timestamp::from_system_time);
 			println!(
 				"{}: #deps {}, deps mtime {} ({})",
 				path,
 				deps.deps().len(),
-				nanos,
-				if deps.mtime().map_or(true, |t| Some(t) < mtime()) {
+				deps.mtime().map_or(0, Timestamp::to_nanos),
+				if mtime.is_none() || mtime > deps.mtime() {
 					"STALE"
 				} else {
 					"VALID"
-				}
+				},
 			);
 			for dep in deps.deps() {
 				println!("    {}", dep);
